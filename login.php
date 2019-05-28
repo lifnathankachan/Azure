1a1276468b6247fe79a0fb9d3b3113b9a89138ee
lifnathankachan <37173868+lifnathankachan@users.noreply.github.com>
2019-05-27 23:09:36 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,217 +1,28 @@
-<!-- //End-slider-script 
-<!DOCTYPE html>
-<html lang="en" >
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-<head>
-  <meta charset="UTF-8">
-  <title>Log'n Load</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
 
-<link href="css/stylea.css" type="text/css" rel="stylesheet" media="all">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+    </div>
 
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+      <input type="submit" class="fadeIn fourth" value="Log In">
+    </form>
 
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+      <a class="underlineHover" href="#">Forgot Password?</a>
+    </div>
 
-  
-</head>
-
-<body>
-
-  <div class="login">
-  <header class="header">
-    <span class="text">LOGIN</span>
-    <span class="loader"></span>
-  </header>
-  <form class="form" action="show.php" method="post" name="loginForm" >  
-    <input class="input" name="u_name" type="text"">
-    <input class="input" name="u_pass" type="password">
-    <button class="btn" type="submit"></button>
-  </form>
- 
+  </div>
 </div>
-<a  href="signup.php">sign up</a>
-<button class ="resetbtn" type= "reset">Reset it
-</button>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-  
-
-    <script  src="js/index.js"></script>
-
-
-
-
-</body>
-
-</html>-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Medi Plus</title>
-
-
-<head>
-  <meta charset="UTF-8">
-  <title>Log'n Load</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
-
-<link href="css/stylea.css" type="text/css" rel="stylesheet" media="all">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link href="css/style.css" type="text/css" rel="stylesheet" media="all">
-<link href="css/styleb.css" type="text/css" rel="stylesheet" media="all">
-<link href="css/component.css" rel="stylesheet" type="text/css"  />
-	<!-- font-awesome icons -->
-<!-- //Custom Theme files -->
-<!-- js -->
-
-<!-- web fonts -->
-
-<link href="//fonts.googleapis.com/css?family=Josefin+Sans:100,100i,300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
-<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
-<!-- //web fonts -->
-
-</head>
-<body class="cbp-spmenu-push">
-
-
-
-
-
-
-<div class="login">
-  <header class="header">
-    <span class="text">LOGIN</span>
-    <span class="loader"></span>
-  </header>
-  <form class="form" action="show.php" method="post" name="loginForm" >  
-    <input class="input" name="u_name" type="text"">
-   	<input class="input" name="u_pass" type="password"></br>
-    <button class="btn"  type="submit">Login</button></br>
-    <button class="btn"  type="reset">Reset</button>
-    
-  </form>
- 
-</div>
-
-
-
-
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-  
-
-    <script  src="js/index.js"></script>
-
-
-
-
-
-
-
-
-
-
-
-
-	<!-- banner -->
-	<div class="banner">
-		<!-- header -->		
-		<div class="w3ls-header">
-			<div class="container">	
-				<div class="agile-logo">
-					<h1><a href="index.html">Medi Plus</a></h1>
-                   
-				</div>
-                
-				<div class="top-nav">
-					<nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="cbp-spmenu-s2">
-						<h3>Menu</h3>
-						<a href="index.php" class="active">Home</a>
-						<a href="about.php">About us</a>
-						
-						<a href="login.php">login </a>
-                        <a  href="signup.php">sign up</a>
-						<a href="contact.php">Contact Us</a>
-					</nav>  
-                 
-                
-					<div class="main buttonset">	
-						<!-- Class "cbp-spmenu-open" gets applied to menu and "cbp-spmenu-push-toleft" or "cbp-spmenu-push-toright" to the body -->
-						<button id="showRightPush"><img src="images/menu-icon.png" alt=""/></button>
-						<!-- <span class="menu"></span> -->
-					</div>
-					<!-- script-for-drop down -->
-					<script>
-						$( "a.menu-icon" ).click(function() { 
-							$( "ul.nav1" ).slideToggle( 300, function() {
-								// Animation complete.
-							});
-						});
-					</script>
-					<!-- //script-for-dropdown --> 
-					<!-- Classie - class helper functions by @desandro https://github.com/desandro/classie -->
-					<script src="js/classie.js"></script>
-					<script>
-						var menuRight = document.getElementById( 'cbp-spmenu-s2' ),
-						showRightPush = document.getElementById( 'showRightPush' ),
-						body = document.body;
-
-						showRightPush.onclick = function() {
-							classie.toggle( this, 'active' );
-							classie.toggle( body, 'cbp-spmenu-push-toleft' );
-							classie.toggle( menuRight, 'cbp-spmenu-open' );
-							disableOther( 'showRightPush' );
-						};
-
-						function disableOther( button ) {
-							if( button !== 'showRightPush' ) {
-								classie.toggle( showRightPush, 'disabled' );
-							}
-						}
-					</script>
-					<!-- /script-for-menu -->
-				</div>	
-			</div>
-		</div>
-		<!-- //header -->
-		
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-	
-</body>
-</html>
